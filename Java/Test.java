17c8b6366f9422ff0d6cc2e6a619424c77d10802
hdoddapaneni <119036944+hdoddapaneni@users.noreply.github.com>
2023-03-29 16:37:42 +0530

    Add files via upload

---
+++ Java/Test.java
@@ -0,0 +1,16 @@
+package examples;
+
+public class Test {
+    public int multiply(int n1, int n2) {
+
+
+        return n1 * n2;
+    }
+    public int add(int n1,int n2){
+        return n1+n2;
+    }
+
+    public int square(int n1){
+        return n1*n1;
+    }
+}
