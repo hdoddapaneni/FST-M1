7d581c219fe027f9947871aa31448d4cd5347359
hdoddapaneni <119036944+hdoddapaneni@users.noreply.github.com>
2023-03-29 12:09:46 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,11 @@
+REM   Script: Activity1
+REM   create a table in SQL using the CREATE command named salesman
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+
