b9ea93ed721a36f32271c83450c6eac791090558
hdoddapaneni <119036944+hdoddapaneni@users.noreply.github.com>
2023-04-03 11:25:30 +0530

    Add files via upload

---
+++ JUnit/Activities/BankAccount.java
@@ -0,0 +1,21 @@
+package activities;
+
+
+public class BankAccount {
+    private Integer balance;
+
+
+    public BankAccount(Integer initialBalance) {
+        balance = initialBalance;
+    }
+
+
+    public Integer withdraw(Integer amount) throws NotEnoughFundsException {
+        if (balance < amount) {
+            throw new NotEnoughFundsException(amount, balance);
+        }
+        balance -= amount;
+        return balance;
+    }
+}
+
